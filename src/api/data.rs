c2f20746458c1969fc63d4818ad555b72b04e58b
lokesh185 <lokeshkanan185@gmail.com>
2024-02-01 19:06:36 +0530

    feat add compre and midsem datetime parsing

--- src/api/data.rs
+++ src/api/data.rs
@@ -1,13 +1,8 @@
+use std::{error::Error, str::FromStr};
+
 use super::responses::{CourseResponse, SectionResponse, TimeTableResponse};
-use iso8601::DateTime;
+use iso8601::{Date, DateTime};
 use regex;
-#[derive(Debug)]
-pub enum TimingError {
-    BadStringFormat,
-    InvalidTimingFormat,
-    InvalidDay,
-    InvalidTime,
-}
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Day {
@@ -135,6 +130,24 @@ impl Course {
             _ => {}
         }
     }
+    fn update_exam_time(&mut self, exam_times: &Vec<ExamTime>) {
+        exam_times
+            .iter()
+            .filter_map(|f| match f.code.eq(&self.code) {
+                true => Some(f),
+                false => None,
+            })
+            .for_each(|exam_time| match exam_time.exam_type {
+                ExamKind::Midsem => {
+                    self.midsem_date_time =
+                        Some((exam_time.start_date_time, exam_time.end_date_time))
+                }
+                ExamKind::Compre => {
+                    self.compre_date_time =
+                        Some((exam_time.start_date_time, exam_time.end_date_time))
+                }
+            })
+    }
 }
 
 #[derive(Debug, Default)]
@@ -170,12 +183,95 @@ impl TimeTable {
                     }
                 };
             });
+        let exam_times = time_table_response
+            .examTimes
+            .iter()
+            .filter_map(|info| match ExamTime::from_string(info.clone()) {
+                Ok(exam_time) => Some(exam_time),
+                Err(_) => None,
+            })
+            .collect::<Vec<ExamTime>>();
 
+        courses
+            .iter_mut()
+            .for_each(|course| course.update_exam_time(&exam_times));
         Self {
             id: time_table_response.id.clone(),
             name: time_table_response.name.clone(),
             acad_year: time_table_response.acadYear,
             courses: courses,
         }
+    }
+}
+pub enum ExamKind {
+    Midsem,
+    Compre,
+}
+pub struct ExamTime {
+    code: String,
+    exam_type: ExamKind,
+    start_date_time: DateTime,
+    end_date_time: DateTime,
+}
+impl ExamTime {
+    fn from_string(info: String) -> Result<Self, regex::Error> {
+        let re = regex::Regex::new(r"(\w+ \w\d{3})\|(\w{6})\|([^\|]+)\|(.+)")?;
+
+        let Some(caps) = re.captures(&info) else {
+            return Err(regex::Error::Syntax(
+                "required values not found in string".to_string(),
+            ));
+        };
+        dbg!(&caps);
+        Ok(Self {
+            code: match caps.get(1) {
+                Some(capture) => capture,
+                None => {
+                    return Err(regex::Error::Syntax("code not present".to_string()));
+                }
+            }
+            .as_str()
+            .to_string(),
+            exam_type: match match caps.get(2) {
+                Some(capture) => capture,
+                None => {
+                    return Err(regex::Error::Syntax("exam kind not present".to_string()));
+                }
+            }
+            .as_str()
+            {
+                "MIDSEM" => ExamKind::Midsem,
+                "COMPRE" => ExamKind::Compre,
+                _ => {
+                    return Err(regex::Error::Syntax("exam type not vaild".to_string()));
+                }
+            },
+            start_date_time: match caps.get(3) {
+                Some(capture) => match DateTime::from_str(capture.as_str()) {
+                    Ok(date_time) => date_time,
+                    Err(_) => {
+                        return Err(regex::Error::Syntax(
+                            "start time syntax is incorrect".to_string(),
+                        ));
+                    }
+                },
+                None => {
+                    return Err(regex::Error::Syntax("start time not present".to_string()));
+                }
+            },
+            end_date_time: match caps.get(4) {
+                Some(capture) => match DateTime::from_str(capture.as_str()) {
+                    Ok(date_time) => date_time,
+                    Err(_) => {
+                        return Err(regex::Error::Syntax(
+                            "end time syntax is incorrect".to_string(),
+                        ));
+                    }
+                },
+                None => {
+                    return Err(regex::Error::Syntax("end time not present".to_string()));
+                }
+            },
+        })
     }
 }
