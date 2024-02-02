e4302ab1ea4cd2dc3a0fb3b9855c1b9d556d5565
lokesh185 <lokeshkanan185@gmail.com>
2024-02-02 18:06:51 +0530

    feat : add holiday.json and structs for holiday data retrival

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,5 @@
 pub mod client;
 #[allow(non_snake_case)]
-#[allow(dead_code)]
-pub mod responses;
+mod responses;
 
 pub mod data;
--- src/api/responses.rs
+++ src/api/responses.rs
@@ -3,31 +3,22 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug, Default)]
 pub struct TimeTableResponse {
     pub id: String,
-    authorId: String,
     pub name: String,
     degrees: Vec<String>,
-    private: bool,
-    draft: bool,
-    archived: bool,
     year: i32,
     pub acadYear: i32,
     pub sections: Vec<SectionResponse>,
     pub timings: Vec<String>,
     pub examTimes: Vec<String>,
-    warnings: Vec<String>,
-    createdAt: String,
-    lastUpdated: String,
 }
 #[derive(Deserialize, Debug, Default)]
 pub struct SectionResponse {
-    id: String,
     pub courseId: String,
     #[serde(rename = "type")]
     pub type_name: String,
     pub number: i32,
     pub instructors: Vec<String>,
     pub roomTime: Vec<String>,
-    createdAt: String,
 }
 #[derive(Deserialize, Debug, Default)]
 #[serde(transparent)]
@@ -41,4 +32,23 @@ pub struct Course {
     pub id: String,
     pub code: String,
     pub name: String,
+}
+#[derive(Deserialize, Debug, Default)]
+pub struct Holiday {
+    pub name: String,
+    pub day: String,
+}
+#[derive(Deserialize, Debug, Default)]
+pub struct TimeTableChange {
+    pub date: String,
+    pub day: String,
+}
+#[derive(Deserialize, Debug, Default)]
+pub struct HolidayResponse {
+    pub classwork_start: String,
+    pub classwork_end: String,
+    pub midsem_start: String,
+    pub midsem_end: String,
+    pub holidays: Vec<Holiday>,
+    pub time_table_changes: Vec<TimeTableChange>,
 }
